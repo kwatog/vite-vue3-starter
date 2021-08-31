d9c9898577ea766bd1381ea11e92dc39913684c6
kwatog@mbp <mbp@kwatog.com>
2021-09-01 00:57:26 +0800

    add pinia for state management

--- src/stores/user.ts
+++ src/stores/user.ts
@@ -1,10 +1,14 @@
 import { ref, computed } from 'vue'
 import { acceptHMRUpdate, defineStore } from 'pinia'
+import Cookies from 'js-cookie'
 
 export const useUserStore = defineStore('user', () => {
   /**
    * Current named of the user.
    */
+
+  // store/use-user-store.ts
+
   const savedName = ref('')
   const accessToken = ref('')
   const previousNames = ref(new Set<string>())
@@ -12,6 +16,14 @@ export const useUserStore = defineStore('user', () => {
 
   const usedNames = computed(() => Array.from(previousNames.value))
   const otherNames = computed(() => usedNames.value.filter(name => name !== savedName.value))
+
+  // import Cookies from 'js-cookie'
+  /**
+   * Changes the current name of the user and saves the one that was used
+   * before.
+   */
+  accessToken.value = Cookies.get('accessToken')
+  savedName.value = Cookies.get('userName')
 
   /**
    * Changes the current name of the user and saves the one that was used
@@ -24,6 +36,7 @@ export const useUserStore = defineStore('user', () => {
       previousNames.value.add(savedName.value)
 
     savedName.value = name
+    Cookies.set('userName', savedName.value.value, { expires: 3 })
   }
 
   function setAccessToken(token: string) {
@@ -31,12 +44,14 @@ export const useUserStore = defineStore('user', () => {
       prevTokens.value.add(accessToken.value)
 
     accessToken.value = token
+    Cookies.set('accessToken', accessToken.value, { expires: 3 })
   }
 
   return {
     setNewName,
     otherNames,
     savedName,
+    accessToken,
     setAccessToken,
   }
 })
